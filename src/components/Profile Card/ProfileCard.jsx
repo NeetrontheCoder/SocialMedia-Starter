2abc281a7ef4fd84a94c220c879159a4f0d3e7d4
Neetu <upneetneetu@gmail.com>
2022-09-04 16:18:36 +0200

    profile card done min 40:34

---
+++ src/components/Profile Card/ProfileCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg";
+import "./ProfileCard.css";
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+        <div className="ProfileImages">
+            <img src={Cover} alt="" />
+            <img src={Profile} alt="" />
+        </div>{/* The entire card. */}
+
+        <div className="ProfileName">
+            <span>Neetron the Great</span>
+            <span>Junior Full Stack Developer</span>
+        </div> {/* User name + title */}
+          
+        <div className="followStatus">
+            <hr />
+            <div>
+                  <div className="follow">
+                      <span>8,966</span>
+                      <span>Followers</span>
+                  </div>  
+                  <div className="vl"></div>
+                  <div className="follow">
+                      <span>1</span>
+                      <span>Following</span>
+                  </div>
+              </div>
+              <hr />
+          </div>
+          
+          <span>
+              My Profile 
+          </span>
+    
+      
+      
+      
+    </div>
+  );
+};
+
+export default ProfileCard;
