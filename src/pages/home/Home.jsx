bbacae4b9e6a68a861f482ba096fb5e95ca8dcc6
Neetu <upneetneetu@gmail.com>
2022-09-05 15:25:30 +0200

    finished top section of middle (1:11:02)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import PostSide from '../../components/PostSide/PostSide';
 import ProfileSide from '../../components/profileSide/ProfileSide';
 import './Home.css'
+
 
 const Home = () => {
   return (
     <div className="Home">
-      <ProfileSide/>
-      <div className="postSide">Posts</div>
+      <ProfileSide />
+      <PostSide/>
       <div className="rightSide">Rightside</div>
     </div>
   );
