a4230dd5ef8f7bd50041da2125bf85e2f8aad5b6
Neetu <upneetneetu@gmail.com>
2022-09-06 14:25:37 +0200

    photo pop-up glitch fixed

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,25 +1,24 @@
-import React, {useState,useRef} from 'react'
-import ProfileImage from '../../img/profileImg.jpg'
-import './PostShare.css'
-import { UilScenery } from "@iconscout/react-unicons"
-import { UilPlayCircle } from "@iconscout/react-unicons"
-import {UilLocationPoint } from "@iconscout/react-unicons"
-import {UilSchedule} from "@iconscout/react-unicons"
-
+import React, { useState, useRef } from "react";
+import ProfileImage from "../../img/profileImg.jpg";
+import "./PostShare.css";
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
 
 const PostShare = () => {
-    const [image, setImage] = useState(null)
-    const imageRef = useRef()
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
 
-
-const onImageChange = (event) => { 
+  const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
-        let img = event.target.files[0];
-        setImage({
-            image: URL.createObjectURL(img)
-        })
+      let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
     }
-}
+  };
   return (
     <div className="PostShare">
       <img src={ProfileImage} alt="" />
@@ -27,32 +26,46 @@ const onImageChange = (event) => {
         <input type="text" placeholder="What's happening?" />
 
         <div className="PostOptions">
-                  <div className="option" style={{ color: "var(--photo)" }}
-                  onclick={()=>imageRef.current.click()}>
-                      
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
             <UilScenery />
             Photo
           </div>
-                  <div className="option" style={{ color: "var(--video)" }}>
+          <div className="option" style={{ color: "var(--video)" }}>
             <UilPlayCircle />
             Video
           </div>
-                  <div className="option" style={{ color: "var(--location)" }}>
+          <div className="option" style={{ color: "var(--location)" }}>
             <UilLocationPoint />
             Location
           </div>
-                  <div className="option" style={{ color: "var(--schedule)" }}>
+          <div className="option" style={{ color: "var(--schedule)" }}>
             <UilSchedule />
             Schedule
-                  </div>
-                  <button className="button ps-button">Share</button>
-                  <div style={{display:"none"}}>
-                      <input type="file" name="myImage" ref={imageRef} onChange={onImageChange}/>
-                  </div>
+          </div>
+          <button className="button ps-button">Share</button>
+
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
+      {image && (
+          <div className="previewImage">
+            <UilTimes />
+            
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default PostShare
+export default PostShare;
