19db5b760fcee974248249d8a8673ce5b1c12cc4
Neetu <upneetneetu@gmail.com>
2022-09-07 13:55:07 +0200

    wednesday PostsData added

---
+++ src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postPic1.jpg';
+import postPic2 from '../img/postPic2.jpg';
+import postPic3 from '../img/postPic3.jpg';
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Jimbo',
+        desc: 'Anyone else excited for the new season of Stranger Things?',
+        likes: 10,
+        liked: false,
+    },
+    {
+        img: postPic2,
+        name: 'Sally',
+        desc: 'I love the new season of The Office!',
+        likes: 565,
+        liked: true,
+    },
+
+    { 
+        img: postPic3,
+        name: 'Eric Lee',
+        desc: 'I love the new season of The Lord of the Rings!',
+        likes: 1083,
+        liked: true,
+    },
+];
+
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Posts.css'
+import {PostsData} from '../../data/PostsData'
+
+const Posts = () => {
+  return (
+      <div className="posts">
+          
+    </div>
+  )
+}
+
+export default Posts
