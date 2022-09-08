1f611b349447ab075d9a56e30d4d7d8510ccfe3a
Neetu <upneetneetu@gmail.com>
2022-09-08 13:57:01 +0200

    created TrendCard

---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+        name: "Minions",
+        shares: 97,
+    },
+    {
+        name: "Avengers",
+        shares: 80.5,
+    },
+    {
+        name: "GitHub",
+        shares: 101
+    },
+    {
+        name: "George Best",
+        shares: 3000
+    },
+    {
+        name: "Sookie",
+        shares: 13
+    }
+]
