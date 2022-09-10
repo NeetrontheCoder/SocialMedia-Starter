a358619d4e21c3063c34f9b3e59b972b5bbce155
Neetu <upneetneetu@gmail.com>
2022-09-10 23:25:36 +0200

    profile modal started

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './InfoCard.css'
 import { UilPen } from '@iconscout/react-unicons'
+import ProfileModel from '../ProfileModel/ProfileModel'
 
 const InfoCard = () => {
+
+
+    const [modalOpened, setModalOpened] = React.useState(false)
   return (
     <div className="InfoCard">
       <div className="infoHead">
         <h4>Your Info</h4>
         <div>
-          <UilPen width="2rem" height="1.2rem" />
+          <UilPen width="2rem" height="1.2rem" onClick={() => setModalOpened(true)} />
+          <ProfileModel opened={modalOpened} onClose={() => setModalOpened(false)} />
         </div>
       </div>
 
